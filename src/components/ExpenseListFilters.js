518de0e37d9a5db30d7dde0ac5e19be2f91b8918
Felipe Hassad P Rafful <felipe.rafful@gmail.com>
2018-01-18 19:35:33 -0200

    Adjustments in webpack configuration for production

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -74,4 +74,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(ExpenseListFilters);
+export default connect(mapStateToProps, mapDispatchToProps)(ExpenseListFilters);
